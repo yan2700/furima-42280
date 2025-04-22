3014af7b46ff8746ed85bf12470ed2722103122d
yan2700 <tomoyahyuga1213@gmail.com>
2025-04-22 18:05:27 +0900

    Set root route to items#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   
   # Defines the root path route ("/")
-  root "posts#index"
-  resources :prototypes
+  root "items#index"   # 
+  
+  resources :items     # ←item
 end
