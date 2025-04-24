b62419a20e71db11c06151f3e48d3f89a0c6ce8c
yan2700 <tomoyahyuga1213@gmail.com>
2025-04-24 11:49:43 +0900

    Fix: enable root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'items/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   
   # Defines the root path route ("/")
-  root "items#index"   # 
+  root "items#index"   
   
-  resources :items     # ←item
+  resources :items     
 end
